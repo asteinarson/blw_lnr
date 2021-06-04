9c5ff908a200f9bc681cd1dd0925052980564196
Arne Steinarson <asteinarson@gmail.com>
2021-06-04 12:42:55 +0200

    Launching node w extra flag, from JS instead (bin section)

--- src/echo_args.ts
+++ src/echo_args.ts
@@ -1,6 +1,16 @@
 
+export {};
 
-console.log("cwd: " + process.cwd() );
 console.log("My args: ");
 console.log(process.argv);
 
+async function sleep(ms:number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+for (let ix = 0; ix < 10; ix++) {
+    console.log("ix: " + ix);
+    await sleep(500);
+}
--- src/launch_lnr_er.ts
+++ src/launch_lnr_er.ts
@@ -1,11 +1,12 @@
-
+#!/usr/bin/env node 
 import {spawn} from 'child_process';
 
-let args = ["--experimental-specifier-resolution=node", ...process.argv.slice(2)];
+let args = ["--experimental-specifier-resolution=node", "./lib/lnr.js", ...process.argv.slice(2)];
 let r = spawn("node", args);
 
 r.stdout.on("data", data => { process.stdout.write(data.toString()); });
 r.stderr.on("data", data => { process.stderr.write(data.toString()); });
-r.on("close", code => {console.log("Closed with EC: "+code)} );
+r.on("close", code => {process.exit(code)} );
 r.on("error", err => {console.log("Error: "+err)} );
 
+ 
