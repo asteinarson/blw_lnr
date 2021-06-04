518feaa161bcaae38d424c64c0489cd980c73dcc
Arne Steinarson <asteinarson@gmail.com>
2021-06-04 12:59:23 +0200

    Fix for missing __dirname in ESM

--- src/launch_lnr_er.ts
+++ src/launch_lnr_er.ts
@@ -1,12 +1,15 @@
-#!/usr/bin/env node 
-import {spawn} from 'child_process';
+#!/usr/bin/env node
+import { spawn } from 'child_process';
+import path from 'path';
 
-let args = ["--experimental-specifier-resolution=node", "./lib/lnr.js", ...process.argv.slice(2)];
+const esm_dirname = path.dirname( process.argv[1] );
+let args = [ "--experimental-specifier-resolution=node",
+    path.join(esm_dirname, "lnr.js"),
+    ...process.argv.slice(2)];
 let r = spawn("node", args);
 
 r.stdout.on("data", data => { process.stdout.write(data.toString()); });
 r.stderr.on("data", data => { process.stderr.write(data.toString()); });
-r.on("close", code => {process.exit(code)} );
-r.on("error", err => {console.log("Error: "+err)} );
+r.on("close", code => { process.exit(code) });
+r.on("error", err => { console.log("Error: " + err) });
 
- 
