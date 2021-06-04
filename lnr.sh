83e3177d4081cff35632abae5cef0678701602ab
Arne Steinarson <asteinarson@gmail.com>
2021-06-04 11:39:00 +0200

    Pick up JS path from shell better

--- lnr.sh
+++ lnr.sh
@@ -1,2 +1,11 @@
 #/bin/bash
-/usr/bin/env node --experimental-specifier-resolution=node lib/lnr.js "$@"
+# Get directory of calling script, from these two: 
+# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
+# https://unix.stackexchange.com/questions/17499/get-path-of-current-script-when-executed-through-a-symlink
+if [ -L "$0" ]; then
+  DIR="$(dirname "$(readlink -f "$0")")"
+else
+  DIR="$( cd "$( dirname "$0" )" &> /dev/null && pwd )"
+fi
+/usr/bin/env node --experimental-specifier-resolution=node $DIR/lib/lnr.js "$@"
+
